7b3e4477a3e6f6ffc140d8dfa06e8ca2e6f34d4b
supritha deshagoni <deshagonisupritha@gmail.com>
2025-12-15 11:49:16 +0530

    maven_projects

---
+++ Maven_projects/fibonacci/src/main/java/com/skillnext/Fibonacci.java
@@ -0,0 +1,22 @@
+package com.skillnext;
+
+/**
+ * Hello world!
+ *
+ */
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10;   // Print first 10 numbers
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: " + a + " " + b);
+
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+        }
+    }
+}
+
